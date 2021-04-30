52e769326a11dcfff1542ed70f55efdde4145bd4
miguelsofo <miguelsofoharo@gmail.com>
2021-04-29 23:04:59 -0300

    resultados de iv inversa actualiazados

--- svisual/currents.tcl
+++ svisual/currents.tcl
@@ -1,11 +1,11 @@
 #set infile ../currents/ford_iv.plt
 #set outfile  ../out_svisual/ford_iv.plt.csv
 
-#set infile ../currents/back_iv.plt
-#set outfile  ../out_svisual/back_iv.plt.csv
+set infile ../currents/back_iv.plt
+set outfile  ../out_svisual/back_iv.plt.csv
 
-set infile ../currents/alfa.plt
-set outfile  ../out_svisual/alfa.plt.csv
+#set infile ../currents/alfa.plt
+#set outfile  ../out_svisual/alfa.plt.csv
 
 set mydata [load_file $infile]
 export_variables -dataset $mydata -filename $outfile -overwrite
